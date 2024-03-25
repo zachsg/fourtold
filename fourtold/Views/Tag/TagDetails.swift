92a8a903786db2db7d400e72c73f8edd9478d035
zachsg <zsgott@gmail.com>
2024-03-25 16:43:58 -0700

    Cleanup prints.

--- fourtold/Views/Tag/TagDetails.swift
+++ fourtold/Views/Tag/TagDetails.swift
@@ -46,12 +46,6 @@ struct TagDetails: View {
                         t.id == tagStat.id
                     }
 
-                    for t in tags {
-                        print("\(t.id): \(t.title)")
-                    }
-
-                    print(tagStat.id)
-
                     if let tag {
                         print(tag.title)
                         modelContext.delete(tag)
